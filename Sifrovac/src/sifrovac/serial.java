298cd40b0c7e84e0f4dfeb7bc46bde0512bf100b
Student <Student@DESKTOP-PTKTPAI>
2018-02-08 20:54:51 +0100

    testing commit

--- Sifrovac/src/sifrovac/serial.java
+++ Sifrovac/src/sifrovac/serial.java
@@ -1,5 +1,9 @@
 package sifrovac;
 
+import java.io.FileOutputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,5 +15,20 @@ package sifrovac;
  * @author Student
  */
 public class serial {
-    
+    public static void main(String[] args) {
+        int x;
+        //int y;
+        
+        int[] file = new int[x];
+     //   file[0] = ?;
+        
+        try {
+            ObjectOutput out = new ObjectOutputStream(
+                new FileOutputStream("vstupni_retezec.dat"));    
+            out.writeObject(file);
+            out.close();
+        } catch (Exception e) {
+            System.out.println("Neco vyhod:" + e);
+        }
+    }
 }
